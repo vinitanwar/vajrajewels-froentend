7913deb3cc91b455c3bb0bb2bc283a6ce91885f0
vivek <157991083+jonty1231@users.noreply.github.com>
2025-09-22 23:29:27 +0530

    add

--- app/components/utils/apis.js
+++ app/components/utils/apis.js
@@ -1,3 +1,6 @@
-export const baseurl= `http://localhost:8000/api/v1`
-export const baseurl2= `http://localhost:8000/v1`
- export const imgurl= `http://localhost:8000/storage`
+// export const baseurl= `http://localhost:8000/api/v1`
+// export const baseurl2= `http://localhost:8000/v1`
+//  export const imgurl= `http://localhost:8000/storage`
+export const baseurl= `http://admin.vajrajewels.com/api/v1`
+export const baseurl2= `http://admin.vajrajewels.com/v1`
+ export const imgurl= `http://admin.vajrajewels.com/storage`
